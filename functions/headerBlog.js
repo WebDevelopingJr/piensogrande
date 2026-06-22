e297ee958facba3084538b61daa06faa3ea29348
Nestor Castillo <131619700+WebDevelopingJr@users.noreply.github.com>
2026-06-22 18:41:01 -0400

    Add files via upload

--- functions/headerBlog.js
+++ functions/headerBlog.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const header = document.getElementById("header");
 
     header.innerHTML = `
-    <a href='/' style="text-style: none; color: black;"><img src='../images/home/log-pienso-horiz.png' alt='log-pienso' style='width: 110px;' /></a>
+    <a href='/piensogrande' style="text-style: none; color: black;"><img src='../images/home/log-pienso-horiz.png' alt='log-pienso' style='width: 110px;' /></a>
       <ul class="nav-links">
-          <li><a href="/aboutUs.html">Conócenos</a></li>
-          <li><a href="/blog.html">Blog</a></li>
-          <li><a href="/services.html">Como puedo ayudarte</a></li>
+          <li><a href="/piensogrande/aboutUs.html">Conócenos</a></li>
+          <li><a href="/piensogrande/blog.html">Blog</a></li>
+          <li><a href="/piensogrande/services.html">Como puedo ayudarte</a></li>
       </ul>
     <a class="btn btn-primary nav-cta" href="/contact.html">Agenda una sesión</a>
     `;
